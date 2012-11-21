604cc651eede9dbfae0f86f59035ca4313f45982
noday <at1943@gmail.com>
2012-11-21 14:16:16 +0800

    更新sql脚本

--- src/main/resources/cat.sql
+++ src/main/resources/cat.sql
@@ -13,9 +13,9 @@ MySQL - 5.0.22-community-nt : Database - industrywords
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`industrywords` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`cat` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `industrywords`;
+USE `cat`;
 
 /*Table structure for table `role` */
 
